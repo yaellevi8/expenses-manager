a8f6184824c541b49a9cdb20e956a973d34848bd
Efi <efi1397@gmail.com>
2023-09-17 12:58:26 +0300

    FIRST

--- src/App.js
+++ src/App.js
@@ -3,18 +3,6 @@ import './App.css';
 import Table from "./components/Table";
 
 function App() {
-  const [expenses, setExpenses] = useState([]);
-
-  const addExpense = (newExpense) => {
-    setExpenses([...expenses, newExpense]);
-  };
-
-  const deleteExpense = (index) => {
-    const updatedExpenses = [...expenses];
-    updatedExpenses.splice(index, 1);
-    setExpenses(updatedExpenses);
-  };
-
     return (
         <div style={{margin: 'auto'}}>
             <Table/>
